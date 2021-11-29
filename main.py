fd77c655df79f39c755acabebbd65dff5ae59d18
RealSoerensen <patricklykke@live.dk>
2021-11-29 02:36:49 +0100

    Updated main
    Better look

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
-kivy.require('2.0.0')
+kivy.require('1.9.0')
+
 
 def alcohol_in_g(alcohol_in_ml):
     return alcohol_in_ml * 0.789
 
-class MyGridLayout(Widget):
 
+class MyGridLayout(Widget):
     bodyweight = ObjectProperty(None)
     normal_beer = ObjectProperty(None)
     strong_beer = ObjectProperty(None)
@@ -20,56 +21,57 @@ class MyGridLayout(Widget):
     button = ObjectProperty(None)
 
     def calculate(self):
-        
+
         # Get the values from the text boxes
         try:
             bodyweight = int(self.bodyweight.text)
-        except:
+        except Exception:
             bodyweight = 0
         try:
             beer = int(self.normal_beer.text)
-        except:
+        except Exception:
             beer = 0
         try:
             strong_beer = int(self.strong_beer.text)
-        except:
+        except Exception:
             strong_beer = 0
         try:
             shot = int(self.shots.text)
-        except:
+        except Exception:
             shot = 0
         try:
             wine = int(self.wine.text)
-        except:
+        except Exception:
             wine = 0
         try:
             cocktail = int(self.cocktail.text)
-        except:
+        except Exception:
             cocktail = 0
         try:
             rtd = int(self.rtd.text)
-        except:
+        except Exception:
             rtd = 0
-        try: 
-            hours = int(self.hours.text)
-        except:
-            hours = 0
-        
         try:
+            hours = int(self.hours.text)
+        except Exception:
+            hours = 0
+
+        try:
+            procent = 0.70
             # Calculate the alcohol in grams
-            if (self.ids.spinner_id.text) == "Man":
+            if self.ids.spinner_id.text == "Man":
                 procent = 0.70
-            elif (self.ids.spinner_id.text) == "Woman":
+            elif self.ids.spinner_id.text == "Woman":
                 procent = 0.60
 
-            alcohol = (alcohol_in_g(beer * (3.3*4.6)) + 
-            alcohol_in_g(strong_beer*(3.3*5.8)) + 
-            alcohol_in_g(wine*(1.2*12)) + 
-            alcohol_in_g(cocktail*(3.3*4.5)) + 
-            alcohol_in_g(shot*(0.4*40)) + 
-            alcohol_in_g(rtd*(27.5*5)))
+            alcohol = (alcohol_in_g(beer * (3.3 * 4.6)) +
+                       alcohol_in_g(strong_beer * (3.3 * 5.8)) +
+                       alcohol_in_g(wine * (1.2 * 12)) +
+                       alcohol_in_g(cocktail * (3.3 * 4.5)) +
+                       alcohol_in_g(shot * (0.4 * 40)) +
+                       alcohol_in_g(rtd * (27.5 * 5)))
 
-            alcohol_consumed = round(alcohol / (bodyweight*procent)-(0.15*hours), 2)
+            alcohol_consumed = round(alcohol / (bodyweight * procent) - (0.15 * hours), 2)
             if alcohol_consumed < 0:
                 alcohol_consumed = 0
 
@@ -78,10 +80,12 @@ class MyGridLayout(Widget):
         except Exception:
             self.ids.button.text = "Error, please correct your input"
 
+
 class HowDrunkAmIApp(App):
     def build(self):
         HowDrunkAmIApp.title = "How Drunk Am I?"
         return MyGridLayout()
+
 
 if __name__ == "__main__":
     HowDrunkAmIApp().run()
